c0fa3e9d053dec560a73b737f4f04700888efbea
Benoit Pasquier <pasquieb@uci.edu>
2017-12-11 12:19:04 -0800

    Added 5-color palette for BTD paper

---
+++ BTD_5_colors.m
@@ -0,0 +1,8 @@
+function cmap = BTD_5_colors
+% 5-color palette for 5 state paper
+% from 7-color palette
+cmap = [  0   0   0 ; % black (typ state)
+        213  94   0 ; % red (hiAloS state)
+        230 159   0 ; % orange (loAhiS state)
+          0 114 178 ; % blue (hiH state)
+         86 180 233 ] % sky blue (loH state)
