02f69b8759287ef249164d1d3bc459d9d4f077b2
Benoit Pasquier <pasquieb@uci.edu>
2017-12-11 17:58:05 -0800

    Changed colors to colorbrewer purple green

--- BTD_5_colors.m
+++ BTD_5_colors.m
@@ -3,9 +3,7 @@ function cmap = BTD_5_colors
 % from 7-color palette of doi:10.1038/nmeth.1618
 % author: Bang Wong
 cmap = [  0   0   0 ; % black (typ state)
-        208  28 139 ; % dark purple (hiAloS state)
-        241 182 218 ; % light purple (loAhiS state)
-        184 225 134 ; % light green (loH state)
-         77 172  38 ] / 255;% dark green (hiH state)
-
-
+        123  50 148 ; % dark purple (hiAloS state)
+        194 165 207 ; % light purple (loAhiS state)
+        166 219 160 ; % light green (loH state)
+          0 136  55 ] / 255;% dark green (hiH state)
