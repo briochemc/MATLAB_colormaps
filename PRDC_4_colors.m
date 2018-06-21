f8b28ac91b66eed5dfad4d55fc24a0856a759ece
Benoit Pasquier <pasquieb@uci.edu>
2018-06-21 11:59:12 -0700

    Added 4-color palette for PRDC

---
+++ PRDC_4_colors.m
@@ -0,0 +1,8 @@
+function cmap = PRDC_4_colors
+% 4-color palette for preformed, regenerated, recycled, and dark DFe.
+% from 7-color palette of doi:10.1038/nmeth.1618
+% author: Bang Wong
+cmap = [  0 109 219 ; % blue (preformed)
+         36 255  36 ; % green (regenerated)
+         36  36  36 ; % dark gray (dark)
+         255 109 119 ] / 255;% pink/purple (recycled)
